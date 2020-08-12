876f4fb4dd061e915d0c5bba62fd02cc4805f01c
Unknown <jonathandingemanse@kliksafe.nl>
2020-08-12 22:17:30 +0200

    Use Alman cs

--- source/physics/boundary_intersect.h
+++ source/physics/boundary_intersect.h
@@ -211,20 +211,6 @@ struct boundary_intersect
 
 
 
-						/*if (this_particle.kin_energy <= E_TH)
-						{
-							deposition_prob = 0;
-						}
-						else if (this_particle.kin_energy < E_MAX)
-						{
-							deposition_prob = SIGMA_MAX * (1 - ((E_MAX - this_particle.kin_energy) / std::pow(E_MAX - E_TH, 2)) );
-						}
-						else
-						{
-							deposition_prob = SIGMA_MAX * std::exp(-(this_particle.kin_energy - E_MAX) / LAMBDA_0);
-						}*/
-
-						// Jonathan cs
 						if (this_particle.kin_energy <= E_TH)
 						{
 							deposition_prob = 0;
@@ -235,8 +221,22 @@ struct boundary_intersect
 						}
 						else
 						{
-							deposition_prob = E_MAX*std::log(this_particle.kin_energy / 6.621829941) / this_particle.kin_energy;
+							deposition_prob = SIGMA_MAX * std::exp(-(this_particle.kin_energy - E_MAX) / LAMBDA_0);
 						}
+
+						// Jonathan cs
+						/*if (this_particle.kin_energy <= E_TH)
+						{
+							deposition_prob = 0;
+						}
+						else if (this_particle.kin_energy < E_MAX)
+						{
+							deposition_prob = SIGMA_MAX * (1 - ((E_MAX - this_particle.kin_energy) / std::pow(E_MAX - E_TH, 2)) );
+						}
+						else
+						{
+							deposition_prob = E_MAX*std::log(this_particle.kin_energy / 6.621829941) / this_particle.kin_energy;
+						}*/
 
 						// Winters cross section
 						/*const real E_MAX_w = 100;
